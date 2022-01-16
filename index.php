d3622f2cc4da0c9ba763be783e8c1d0e773832d5
S_ICM <butha-puk@hotmail.com>
2022-01-16 15:04:03 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,86 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <style>
+      table {
+        font-family: arial, sans-serif;
+        border-collapse: collapse;
+        width: 100%;
+      }
+      
+      td, th {
+        border: 1px solid #dddddd;
+        text-align: left;
+        padding: 8px;
+      }
+      
+      tr:nth-child(even) {
+        background-color: #dddddd;
+      }
+      </style>
+</head>
+<body>
+
+<h2>KTBKK Hack</h2>
+<iframe width="100%" height="300" src="https://www.youtube.com/embed/WPkbbn8yzl0" title="The Cab Angel" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<table>
+  <tr>
+    <th>name</th>
+    <th>Contact</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td><a href="https://rights.xn--33-nqia4jubqa0kcg0o.com/">เราชนะ</a></td>
+    
+  </tr>
+  <tr>
+    
+    <td>2</td>
+    <td><a href="https://rights.xn--b3c4a2a6ch6f.com/rights">ม33</td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td><a href="https://findtel.herokuapp.com/">ข้อมูลการฉีดวัคซีน</td>
+  </tr>
+  <tr>
+    <td>4</td>
+    <td><a href="
+        https://eservices.nhso.go.th/eServices/mobile/login.xhtml">สปสช</td>
+  </tr>
+  <tr>
+    <td>5</td>
+    <td><a href="https://checkip.thaiware.com/">Check IP</td>
+  </tr>
+  <tr>
+    <td>6</td>
+    <td><a href="https://community.spiceworks.com/tools/ip-lookup/">IP LookUp</td>
+  </tr>
+  <tr>
+    <td>7</td>
+    <td><a href="http://www.inv.p2.police.go.th/links/">Police Investigation</td>
+  </tr>
+  <tr>
+    <td>8</td>
+    <td><a href="http://pol-tech.info/login">Pol Tech</td>
+  </tr>
+  <tr>
+    <td>9</td>
+    <td><a href="http://polisvpn.com/">Polis VPN</td>
+  </tr>
+  <tr>
+    <td>10</td>
+    <td><a href="http://172.31.191.72/">Crime</td>
+  </tr>
+  <tr>
+    <td>Current IP Address:</td>
+    <td><?php
+      echo $_SERVER['REMOTE_ADDR'];
+      ?></td>
+    
+  </tr>
+</table>
+
+</body>
+</html>
